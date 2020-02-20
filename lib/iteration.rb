a4d61e9fda7193f4c20317db7a9da15aedb5e93f
Don Mallory <dmm333@gmail.com>
2020-02-20 22:58:57 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -15,6 +15,7 @@ def find_greater_pair(src)
 
   while pair_index < src.length do
       larger_numbers << src[pair_index][0] >= src[pair_index][1] ? src[pair_index][0] : src[pair_index][1]
+      pair_index += 1
   end
   pair_index
 end
